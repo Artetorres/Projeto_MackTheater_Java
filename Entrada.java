9b716334ba94970d05f7d5559e2ab37bcac9baf4
João Henrique Pereira Amaral <joaohenpereira@gmail.com>
2025-11-05 00:04:20 +0000

    Projeto N2

---
+++ Entrada.java
@@ -0,0 +1,5 @@
+public abstract class Entrada {
+    private int numeroDoAssento;
+
+    public abstract double calculaValor();
+}
---
+++ EntradaInteira.java
@@ -0,0 +1,4 @@
+public class EntradaInteira extends Entrada {
+    private int numeroDoAssento;
+
+}
